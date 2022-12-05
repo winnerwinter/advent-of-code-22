cf7a487ae1a86474d610f678aa94cced1253ded1
Danny Deng-Winter <dannydeng7@gmail.com>
2022-12-04 18:08:46 -0800

    day04 data

---
+++ src/Day04.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    fun part1(): Int {
+        val testInput = readInput("Day04_test")
+        val test_ans = 0
+        check(test_ans == 0)
+
+        val input = readInput("Day04")
+        val ans = 0
+        return ans
+    }
+
+    fun part2(): Int {
+        val testInput = readInput("Day04_test")
+        val test_ans = 0
+        check(test_ans == 0)
+
+        val input = readInput("Day04")
+        val ans = 0
+        return ans
+    }
+
+    println(part1())
+    println(part2())
+}
