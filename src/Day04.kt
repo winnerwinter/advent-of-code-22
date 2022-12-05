14afec7040c43a539c18d84efa12629c47a5ea9d
Danny Deng-Winter <dannydeng7@gmail.com>
2022-12-05 10:19:42 -0800

    day04 pt2

--- src/Day04.kt
+++ src/Day04.kt
@@ -11,26 +11,33 @@ fun main() {
     fun isOverlap(elfA: IntRange, elfB: IntRange): Boolean =
         (elfA intersect elfB).let { it == elfA.toSet() || it == elfB.toSet() }
 
-    fun countFreeloadingPartners(elves: List<Pair<IntRange, IntRange>>): Int =
-        elves.sumOf { (elfA, elfB) -> if (isOverlap(elfA, elfB)) 1 else 0 as Int }
+    fun containsOverlap(elfA: IntRange, elfB: IntRange): Boolean =
+        (elfA intersect elfB).isNotEmpty()
+
+    fun countFreeloadingPartners(
+        elves: List<Pair<IntRange, IntRange>>,
+        determineFreeloading: (IntRange, IntRange) -> Boolean
+    ): Int =
+        elves.sumOf { (elfA, elfB) -> if (determineFreeloading(elfA, elfB)) 1 else 0 as Int }
 
     fun part1(): Int {
         val testInput = readInput("Day04_test")
-        val test_ans = countFreeloadingPartners(testInput.map { it.toElfPair() })
+        val test_ans = countFreeloadingPartners(testInput.map { it.toElfPair() }, ::isOverlap)
         check(test_ans == 2)
 
         val input = readInput("Day04")
-        val ans = countFreeloadingPartners(input.map { it.toElfPair() })
+        val ans = countFreeloadingPartners(input.map { it.toElfPair() }, ::isOverlap)
         return ans
     }
 
+
     fun part2(): Int {
         val testInput = readInput("Day04_test")
-        val test_ans = 0
-        check(test_ans == 0)
+        val test_ans = countFreeloadingPartners(testInput.map { it.toElfPair() }, ::containsOverlap)
+        check(test_ans == 4)
 
         val input = readInput("Day04")
-        val ans = 0
+        val ans = countFreeloadingPartners(input.map { it.toElfPair() }, ::containsOverlap)
         return ans
     }
 
