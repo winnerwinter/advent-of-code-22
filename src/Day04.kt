9db1c3c89ab785598698ec3bf72541f8228e34d4
Danny Deng-Winter <dannydeng7@gmail.com>
2022-12-05 09:39:50 -0800

    day04 pt1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,12 +1,26 @@
 fun main() {
 
+    fun String.toElfPair(): Pair<IntRange, IntRange> {
+        val (elfA, elfB) = this.split(",").map { sections ->
+            val (low, high) = sections.split("-")
+            low.toInt()..high.toInt()
+        }
+        return elfA to elfB
+    }
+
+    fun isOverlap(elfA: IntRange, elfB: IntRange): Boolean =
+        (elfA intersect elfB).let { it == elfA.toSet() || it == elfB.toSet() }
+
+    fun countFreeloadingPartners(elves: List<Pair<IntRange, IntRange>>): Int =
+        elves.sumOf { (elfA, elfB) -> if (isOverlap(elfA, elfB)) 1 else 0 as Int }
+
     fun part1(): Int {
         val testInput = readInput("Day04_test")
-        val test_ans = 0
-        check(test_ans == 0)
+        val test_ans = countFreeloadingPartners(testInput.map { it.toElfPair() })
+        check(test_ans == 2)
 
         val input = readInput("Day04")
-        val ans = 0
+        val ans = countFreeloadingPartners(input.map { it.toElfPair() })
         return ans
     }
 
