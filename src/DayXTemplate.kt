8d4cbc7147d800f8db29698bb4e48dcabb893e3a
Danny Deng-Winter <dannydeng7@gmail.com>
2022-12-04 18:05:12 -0800

    Add solution template

---
+++ src/DayXTemplate.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    fun part1(): Int {
+        val testInput = readInput("DayX_test")
+        val test_ans = 0
+        check(test_ans == 0)
+
+        val input = readInput("DayX")
+        val ans = 0
+        return ans
+    }
+
+    fun part2(): Int {
+        val testInput = readInput("DayX_test")
+        val test_ans = 0
+        check(test_ans == 0)
+
+        val input = readInput("DayX")
+        val ans = 0
+        return ans
+    }
+
+    println(part1())
+    println(part2())
+}
