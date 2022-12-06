34ed9c8aba8f7c97c896dffa0214b25e94cdfd7a
Danny Deng-Winter <dannydeng7@gmail.com>
2022-12-05 17:08:01 -0800

    day05 pt1

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,12 +1,56 @@
 fun main() {
 
-    fun part1(): Int {
+    fun parseInput(input: List<String>): Pair<State, List<Instruction>> {
+        val instructionSep = input.indexOf("")
+        val diagram = input.subList(0, instructionSep - 1)
+        val stackLabels = input.subList(instructionSep - 1, instructionSep)
+        val instructions = input.subList(instructionSep + 1, input.size)
+
+        val numStacks = stackLabels.single().last().toString().toInt()
+
+        val stackMap = (1 .. numStacks).associateWith { i ->
+            val diagramI = ((i - 1) * 4) + 1
+            diagram.mapNotNull { it.getOrNull(diagramI)?.takeIf { it != ' ' } }.reversed()
+        }
+
+        return stackMap to instructions
+    }
+
+    fun State.moveBox(from: Int, to: Int): State {
+        val crateId = requireNotNull(this[from]?.last())
+        return this.mapValues { (stackId, stack) ->
+            when (stackId) {
+                from -> stack.dropLast(1)
+                to -> listOf(*stack.toTypedArray(), crateId)
+                else -> stack
+            }
+        }
+    }
+
+    fun State.applyInstruction(instruction: Instruction): State {
+        val instructionParts = instruction.split(" ")
+        val move = instructionParts[1].toInt()
+        val from = instructionParts[3].toInt()
+        val to = instructionParts[5].toInt()
+
+        return (0 until move).fold(this) { acc, _ ->
+            acc.moveBox(from, to)
+        }
+    }
+
+    fun State.compute(instructions: List<Instruction>): String =
+        instructions.fold(this) { acc, instruction -> acc.applyInstruction(instruction) }.let {
+            it.values.map { it.last() }.joinToString("")
+        }
+
+
+    fun part1(): String {
         val testInput = readInput("Day05_test")
-        val test_ans = 0
-        check(test_ans == 0)
+        val test_ans = parseInput(testInput).let { (state, instructions) -> state.compute(instructions) }
+        check(test_ans == "CMZ")
 
         val input = readInput("Day05")
-        val ans = 0
+        val ans = parseInput(input).let { (state, instructions) -> state.compute(instructions) }
         return ans
     }
 
@@ -23,3 +67,6 @@ fun main() {
     println(part1())
     println(part2())
 }
+
+typealias State = Map<Int, List<Char>>
+typealias Instruction = String
